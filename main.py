1a902d5474ffa488e40230da6bd02c55aee64ac2
postalove <piRR80@proton.me>
2024-08-02 23:08:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 from dotenv import load_dotenv
-import os
+import os,re
 
 from discord.ext import commands, tasks
 from datetime import datetime, timedelta, timezone
@@ -20,7 +20,25 @@ async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
     await bot.tree.sync()
+# b23
+@bot.event
+async def on_message(message):
+    pattern = r'b23\.tv/\S*'
+    match = re.search(pattern, message.content)
+    embed = discord.Embed(description=f"{message.author.mention}请不要发送短链接！",color=0xff0000)
+    if match:
+        await message.delete()
+        await message.channel.send(embed=embed, delete_after=2)
 
+
+@bot.event
+async def on_message_edit(before, after):
+    pattern = r'b23\.tv/\S*'
+    match = re.search(pattern, after.content)
+    embed = discord.Embed(description=f"{before.author.mention}请不要发送短链接！",color=0xff0000)
+    if match:
+        await after.delete()
+        await after.channel.send(embed=embed, delete_after=2)
 
 @bot.tree.command(name='back_to_top',description='返回首条消息的链接')
 async def back_to_top(interaction: discord.Interaction):
