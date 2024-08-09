aaf43f3533edcba487e44e20d88ac54eb72535f1
postalove <piRR80@proton.me>
2024-08-09 11:09:46 +0800

    .

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import discord
+import interactions
+from interactions import Client,Intents,listen,slash_command,SlashContext
+import interactions.api.events as Events
 from dotenv import load_dotenv
 import os,re
 
-from discord.ext import commands, tasks
-from datetime import datetime, timedelta, timezone
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='%', intents=intents)
+
+
+bot = Client(intents=Intents.ALL)
 
 
 env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
@@ -15,50 +16,65 @@ load_dotenv(dotenv_path=env_path)
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
 
-@bot.event
-async def on_ready():
+@listen(Events.Ready)
+async def on_ready(event):
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
-    await bot.tree.sync()
+    
 # b23
-@bot.event
-async def on_message(message):
+@listen(Events.MessageCreate)
+async def on_message_create(event:Events.MessageCreate):
+    message=event.message
     pattern = r'b23\.tv/\S*'
     match = re.search(pattern, message.content)
-    embed = discord.Embed(description=f"{message.author.mention}请不要发送短链接！",color=0xff0000)
+    embed = interactions.Embed(description=f"{message.author.mention}请不要发送短链接！",color=0xff0000)
     if match:
         await message.delete()
         await message.channel.send(embed=embed, delete_after=2)
 
 
-@bot.event
-async def on_message_edit(before, after):
+@listen(Events.MessageUpdate)
+async def on_message_edit(event:Events.MessageUpdate):
+
+    before=event.before
+    after=event.after
     pattern = r'b23\.tv/\S*'
     match = re.search(pattern, after.content)
-    embed = discord.Embed(description=f"{before.author.mention}请不要发送短链接！",color=0xff0000)
+    embed = interactions.Embed(description=f"{before.author.mention}请不要发送短链接！",color=0xff0000)
     if match:
         await after.delete()
         await after.channel.send(embed=embed, delete_after=2)
 
-@bot.tree.command(name='back_to_top',description='返回首条消息的链接')
-async def back_to_top(interaction: discord.Interaction):
-    async for mess in interaction.channel.history(limit=1, oldest_first=True):
-        link = mess.jump_url
-    await interaction.response.send_message(link)
 
-@bot.event
-async def on_reaction_add(reaction,user):
-    embed=discord.Embed(description=f'{user.mention} added {reaction.emoji} [here]({reaction.message.jump_url})',color=	0x008000)
+@listen(Events.MessageReactionAdd)
+async def on_reaction_add(event:Events.MessageReactionAdd):
+    user=event.author
+    reaction=event.emoji
+    jump_url=event.message.jump_url
+    embed=interactions.Embed(description=f'{user.mention} added {reaction.name} [here]({jump_url})',color=	0x008000)
     guild=bot.get_guild(1252887256327520287)
     log=guild.get_channel(1269303446230798411)
     await log.send(embed=embed)
 
-@bot.event
-async def on_reaction_remove(reaction,user):
-    embed=discord.Embed(description=f'{user.mention} removed {reaction.emoji} [here]({reaction.message.jump_url})',color=0xff0000)
+@listen(Events.MessageReactionRemove)
+async def on_reaction_remove(event:Events.MessageReactionRemove):
+    user=event.author
+    reaction=event.emoji
+    jump_url=event.message.jump_url
+    embed=interactions.Embed(description=f'{user.mention} removed {reaction.name} [here]({jump_url})',color=0xff0000)
     guild=bot.get_guild(1252887256327520287)
     log=guild.get_channel(1269303446230798411)
     await log.send(embed=embed)
 
 
-bot.run(TOKEN)
+
+
+@slash_command(name='back-to-top', description='返回顶部')
+async def back_to_top(ctx: SlashContext):
+    await ctx.defer()
+    async for mess in ctx.channel.history(limit=None):
+        last_message = mess
+    link = last_message.jump_url
+    await ctx.respond(content=f'[返回顶部]({link})')
+
+bot.start(TOKEN)
