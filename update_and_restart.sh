2ca71204ba0ae7c5497f2cdb14800813dba0ebe8
postalove <piRR80@proton.me>
2024-08-14 10:32:13 +0800

    Update update_and_restart.sh

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -3,8 +3,6 @@
 echo "Pulling latest changes from Git..."
 git pull
 
-echo "Installing any new dependencies..."
-pip install -r requirements.txt  # Assuming you have a requirements.txt file
 
 echo "Restarting the Discord bot..."
 pkill -f main.py  # Replace bot_script.py with your bot's main script name
