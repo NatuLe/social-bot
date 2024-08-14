e35f79fa233637bb054825daf578463e75d785c7
postalove <piRR80@proton.me>
2024-08-14 10:36:04 +0800

    Update update_and_restart.sh

--- update_and_restart.sh
+++ update_and_restart.sh
@@ -5,4 +5,4 @@ git pull
 
 echo "Restarting the Discord bot..."
 pkill -f main.py  # Replace bot_script.py with your bot's main script name
-python3 main.py  # Replace bot_script.py with your bot's main script name
+python3 main.py & # Replace bot_script.py with your bot's main script name
